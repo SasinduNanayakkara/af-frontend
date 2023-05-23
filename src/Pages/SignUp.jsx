b346baff0a8c220bad3d2ace3c838079005877cd
shavidiniatsi <shavidiniatsi@gmail.com>
2023-05-24 01:06:07 +0530

    Completed sign up page

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -3,7 +3,7 @@ import Footer from "../Components/Footer";
 import LandingImg from "../Assets/Landing-home.svg";
 import LogoLightPng from "../Assets/LogoLanding.svg";
 import image from "../Assets/Pages-Vectors/signUp.svg";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -44,6 +44,8 @@ const Header = () => {
 };
 
 const Card = () => {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
   const [userType, setUserType] = useState("Client");
   const [formValues, setFormValues] = useState({
     prefix: "Mr.",
@@ -54,6 +56,18 @@ const Card = () => {
     password: "",
     phone: "",
   });
+
+  const validateEmail = () => {
+    const emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!emailPattern.test(email)) {
+      setEmailError("Invalid email address");
+      return false;
+    }
+
+    setEmailError("");
+    return true;
+  };
 
   const handleUserTypeChange = (e) => {
     setUserType(e.target.value);
@@ -69,42 +83,43 @@ const Card = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (validateEmail()) {
+      // Proceed with form submission
+    }
     // Handle form submission logic here
     // Access form values from the `formValues` state object
   };
 
   return (
-    <div className="flex">
-      <div className="bg-white rounded-l-lg overflow-hidden shadow-md flex-1 ml-32 mt-10">
+    <div className="flex mb-10">
+      <div className="bg-white rounded-l-lg overflow-hidden shadow-md flex-1 ml-32 mt-8">
         {/* Content for the left card */}
         <img
           src={image}
           alt="Left Card Image"
           className="w-96 h-96 mx-auto mt-4"
         />
-        <p className="font-inter text-ashforsignup text-center ml-20 mr-20 mb-4">
+        <p className="font-inter text-ashforsignup text-center ml-20 mr-20">
           Our team of experienced consultants is committed to helping you
           achieve your goals and maximize your success. Whether you are a small
           business owner, an entrepreneur, or an individual looking to improve
           your personal or professional life, we are here to help.
         </p>
       </div>
-      <div className="bg-ash rounded-r-lg overflow-hidden shadow-md flex-1 mr-32 mt-10">
+      <div className="bg-ash rounded-r-lg overflow-hidden shadow-md flex-1 mr-32 mt-8">
         {/* Content for the right card */}
-        <h1 className="text-black font-bold text-center mt-12 text-4xl">
-          Join with Consult Therapy
+        <h1 className="text-black font-bold text-center mt-8 text-3xl">
+          Join With Consult Therapy
         </h1>
-        <form className="flex flex-wrap ml-10 mt-10" onSubmit={handleSubmit}>
-          <div className="w-1/3">
-            <label
-              htmlFor="prefix"
-              className="text-ashforsignup font-bold mb-2"
-            >
+        <form className="flex flex-wrap ml-14 mr-14 mt-8 justify-center" onSubmit={handleSubmit}>
+          <div className="w-1/3 pr-2">
+            <label htmlFor="prefix" className="text-formLable font-semibold mb-2">
               Prefix
             </label>
             <select
               id="prefix"
-              className="border-2 border-ashforsignup rounded px-2 py-2 mb-4 mt-1 w-44"
+              className="border border-formLable rounded px-2 py-2 mb-4 mt-1 w-full bg-ash"
               value={formValues.prefix}
               onChange={handleInputChange}
             >
@@ -113,46 +128,40 @@ const Card = () => {
               <option value="Ms.">Ms.</option>
             </select>
           </div>
-          <div className="w-1/3">
+          <div className="w-1/3 pl-2 pr-2 ">
             <label
               htmlFor="firstName"
-              className="text-ashforsignup font-bold mb-2"
+              className="text-formLable font-semibold mb-2"
             >
               First Name
             </label>
             <input
               type="text"
               id="firstName"
-              className="border-2 border-ashforsignup rounded px-2 py-2 mb-4 mt-1 w-44"
+              className="border border-formLable rounded px-2 py-2 mb-4 mt-1 w-full bg-ash"
               value={formValues.firstName}
               onChange={handleInputChange}
             />
           </div>
-          <div className="w-1/3">
-            <label
-              htmlFor="lastName"
-              className="text-ashforsignup font-bold mb-2"
-            >
+          <div className="w-1/3 pl-2">
+            <label htmlFor="lastName" className="text-formLable font-semibold mb-2">
               Last Name
             </label>
             <input
               type="text"
               id="lastName"
-              className="border-2 border-ashforsignup rounded px-3 py-2 mb-4 mt-1 w-44"
+              className="border border-formLable rounded px-3 py-2 mb-4 mt-1 w-full bg-ash"
               value={formValues.lastName}
               onChange={handleInputChange}
             />
           </div>
-          <div className="w-1/2">
-            <label
-              htmlFor="userType"
-              className="text-ashforsignup font-bold mb-2"
-            >
+          <div className="w-1/2 pr-2">
+            <label htmlFor="userType" className="text-formLable font-semibold mb-2">
               User Type
             </label>
             <select
               id="userType"
-              className="border-2 border-ashforsignup rounded px-2 py-2 mb-4 mt-1 w-72"
+              className="border border-formLable rounded px-2 py-2 mb-4 mt-1 w-full bg-ash"
               value={formValues.userType}
               onChange={handleInputChange}
             >
@@ -160,60 +169,64 @@ const Card = () => {
               <option value="Consultant">Consultant</option>
             </select>
           </div>
-          <div className="w-1/2">
-            <label htmlFor="email" className="text-ashforsignup font-bold mb-2">
-              Email
+          <div className="w-1/2 pl-2">
+            <label htmlFor="password" className="text-formLable font-semibold mb-2">
+              Password
             </label>
             <input
-              type="email"
-              id="email"
-              className="border-2 border-ashforsignup rounded px-3 py-2 mb-4 mt-1 w-72"
+              type="password"
+              id="password"
+              className="border border-formLable rounded px-2 py-2 mb-4 mt-1 w-full bg-ash"
+              value={formValues.password}
+              onChange={handleInputChange}
             />
           </div>
           {formValues.userType === "Consultant" && (
             <>
-              <div className="w-1/2">
+              <div className="w-1/2 pr-2">
                 <label
-                  htmlFor="password"
-                  className="text-ashforsignup font-bold mb-2"
+                  htmlFor="email"
+                  className="text-formLable font-semibold mb-2"
                 >
-                  Password
+                  Email
                 </label>
                 <input
-                  type="password"
-                  id="password"
-                  className="border-2 border-ashforsignup rounded px-3 py-2 mb-4 mt-1 w-72"
-                  value={formValues.password}
-                  onChange={handleInputChange}
+                  type="email"
+                  id="email"
+                  className="border border-formLable rounded px-2 py-2 mb-4 mt-1 w-full bg-ash"
+                  onBlur={validateEmail}
                 />
               </div>
-              <div className="w-1/2">
+              <div className="w-1/2 pl-2">
                 <label
                   htmlFor="phone"
-                  className="text-ashforsignup font-bold mb-2"
+                  className="text-formLable font-semibold mb-2"
                 >
                   Phone
                 </label>
                 <input
                   type="phone"
                   id="phone"
-                  className="border-2 border-ashforsignup rounded px-3 py-2 mb-4 mt-1 w-72"
+                  className="border border-formLable rounded px-2 py-2 mb-4 mt-1 w-full bg-ash"
                   value={formValues.phone}
                   onChange={handleInputChange}
                 />
               </div>
               <div className="w-full">
-          <label htmlFor="location" className="text-ashforsignup font-bold mb-2">
-            Location
-          </label>
-          <input
-            type="text"
-            id="location"
-            className="border-2 border-ashforsignup rounded px-3 py-2 mb-4 mt-1 w-full"
-            value={formValues.location}
-            onChange={handleInputChange}
-          />
-        </div>
+                <label
+                  htmlFor="location"
+                  className="text-formLable font-semibold mb-2"
+                >
+                  Location
+                </label>
+                <input
+                  type="text"
+                  id="location"
+                  className="border border-formLable rounded px-2 py-2 mb-4 mt-1 w-full bg-ash"
+                  value={formValues.location}
+                  onChange={handleInputChange}
+                />
+              </div>
             </>
           )}
           <div className="w-full flex justify-center">
@@ -225,15 +238,14 @@ const Card = () => {
             </button>
           </div>
         </form>
-        <div className="text-center mt-4">
-        <p>
-          Already have an account?{' '}
-          <Link to="/login" className="text-ashforsignup font-bold">
-            Log In
-          </Link>
-        </p>
-      </div>
-
+        <div className="text-center mt-4 mb-8">
+          <p>
+            Already have an account?{" "}
+            <Link to="/login" className="text-ashforsignup font-bold">
+              Log In
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
       'black': '#000000',
       'orange': '#F5A623',
       'ash': '#F4F4F4',
-      'ashforsignup':'#646464'
+      'ashforsignup':'#646464',
+      'formLable' : '#8B8B8B'
     },
     fontFamily:{
       'inter': ['Inter', 'sans-serif'],
