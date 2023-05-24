78b5d1fbadcdcd3d97f772f1666226381bdbfe00
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-05-25 04:05:51 +0530

    Initialize the route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,16 +5,17 @@ import Landing from '../Pages/landing'
 import Articles from '../Pages/Articles'
 import SignUp from '../Pages/SignUp'
 import SignIn from '../Pages/SignIn'
+import Announcements from '../Pages/Announcements'
 
 function PageRoutes() {
   return (
     <Router>
         <Routes>
             <Route path="/" element={<Landing/>}/>
-            <Route path='/home' element={<Home />} />
-            <Route path='/articles' element={<Articles/>} />
+            <Route path='/home' element={<Articles/>} />
             <Route path='/signup' element={<SignUp/>}/>
             <Route path='/signin' element={<SignIn/>}/>
+            <Route path='/announcement' element={<Announcements/>} />
         </Routes>
     </Router>
   )
