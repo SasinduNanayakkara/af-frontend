fb466189a45c2c656c3ca578abd779f76e525c8b
Ravindu-Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2023-05-27 11:12:40 +0530

    home page finished

--- src/Pages/common/Home.jsx
+++ src/Pages/common/Home.jsx
@@ -1,53 +1,89 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import ArticleCard from "../../Components/ArticleCard";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Articles() {
-  const articleData = [
-    {
-      author: "Sasindu Nanayakkara",
-      designation: "Emergency Medicine Specialists",
-      title: "Each year management consultants in the United",
-      description:
-        "States receive more than $2 billion for their services.1 Much of this money pays for impractical data and poorly implemented recommendations.2 ",
-      rating: 4,
-      profilePic: "",
-      articlePic: "",
-      article: "Consulting is defined as the practise of providing a third party with expertise on a matter in exchange for a fee. The service can involve either advisory or implementation services. For the consultant, taking an independent and unbiased stance on an issue is central to their role. A consultant can, in principle, service any sector. Over the past decades, the term has become synonymous with business advisory – which focuses mostly on business strategy, management, organisation, operational processes and technology. The history of the consulting industry traces back to the late nineteenth century, with the founding of the world’s first modern consulting organisations, also known as business advisory firms. In the early days of the consulting industry (starting in the US and later crossing over to Europe and the rest of the world) the first consultancies focused their advisory services mainly on solving technical and financial concerns. Today, over 300,000 consulting firms in the UK alone – of which the majority are sole proprietorships – offer all sorts of different services, spanning across countless fields, disciplines and sectors."
-    },
-    {
-      author: "Susith Rupasinghe",
-      designation: "Emergency Medicine Specialists",
-      title: "Each year management consultants in the United",
-      description:
-        "States receive more than $2 billion for their services.1 Much of this money pays for impractical data and poorly implemented recommendations.2 ",
-      rating: 4,
-      profilePic: "",
-      articlePic: "",
-    },
-    {
-      author: "Sasindu Nanayakkara",
-      designation: "Emergency Medicine Specialists",
-      title: "Each year management consultants in the United",
-      description:
-        "States receive more than $2 billion for their services.1 Much of this money pays for impractical data and poorly implemented recommendations.2 ",
-      rating: 4,
-      profilePic: "",
-      articlePic: "",
-    },
-    {
-      author: "Sasindu Nanayakkara",
-      designation: "Emergency Medicine Specialists",
-      title: "Each year management consultants in the United",
-      description:
-        "States receive more than $2 billion for their services.1 Much of this money pays for impractical data and poorly implemented recommendations.2 ",
-      rating: 4,
-      profilePic: "",
-      articlePic: "",
-    },
-  ];
+  const [articleData, setarticleData] = React.useState([]);
+  // const articleData = [
+  //   {
+  //     author: "Sasindu Nanayakkara",
+  //     designation: "Emergency Medicine Specialists",
+  //     title: "Each year management consultants in the United",
+  //     description:
+  //       "States receive more than $2 billion for their services.1 Much of this money pays for impractical data and poorly implemented recommendations.2 ",
+  //     rating: 4,
+  //     profilePic: "",
+  //     articlePic: "",
+  //     article: "Consulting is defined as the practise of providing a third party with expertise on a matter in exchange for a fee. The service can involve either advisory or implementation services. For the consultant, taking an independent and unbiased stance on an issue is central to their role. A consultant can, in principle, service any sector. Over the past decades, the term has become synonymous with business advisory – which focuses mostly on business strategy, management, organisation, operational processes and technology. The history of the consulting industry traces back to the late nineteenth century, with the founding of the world’s first modern consulting organisations, also known as business advisory firms. In the early days of the consulting industry (starting in the US and later crossing over to Europe and the rest of the world) the first consultancies focused their advisory services mainly on solving technical and financial concerns. Today, over 300,000 consulting firms in the UK alone – of which the majority are sole proprietorships – offer all sorts of different services, spanning across countless fields, disciplines and sectors."
+  //   },
+  //   {
+  //     author: "Susith Rupasinghe",
+  //     designation: "Emergency Medicine Specialists",
+  //     title: "Each year management consultants in the United",
+  //     description:
+  //       "States receive more than $2 billion for their services.1 Much of this money pays for impractical data and poorly implemented recommendations.2 ",
+  //     rating: 4,
+  //     profilePic: "",
+  //     articlePic: "",
+  //   },
+  //   {
+  //     author: "Sasindu Nanayakkara",
+  //     designation: "Emergency Medicine Specialists",
+  //     title: "Each year management consultants in the United",
+  //     description:
+  //       "States receive more than $2 billion for their services.1 Much of this money pays for impractical data and poorly implemented recommendations.2 ",
+  //     rating: 4,
+  //     profilePic: "",
+  //     articlePic: "",
+  //   },
+  //   {
+  //     author: "Sasindu Nanayakkara",
+  //     designation: "Emergency Medicine Specialists",
+  //     title: "Each year management consultants in the United",
+  //     description:
+  //       "States receive more than $2 billion for their services.1 Much of this money pays for impractical data and poorly implemented recommendations.2 ",
+  //     rating: 4,
+  //     profilePic: "",
+  //     articlePic: "",
+  //   },
+  // ];
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(
+        "https://af-backend.azurewebsites.net/api/article/"
+      );
+      
+      const filteredData = response.data.data.map((article) => {
+
+        const newcontennt = article.content.blocks.map((block) =>  block.text);
+        console.log("newcontent",newcontennt);
+        return {
+          author: article.author.firstName+" "+article.author.lastName,
+          designation: article.author.specialization,
+          title: article.title,
+          description: newcontennt.join(" ").substr(0, 100),
+          //random number between 1 and 5
+          rating: Math.floor(Math.random() * 5) + 1,
+          profilePic:"" ,
+          articlePic: "",
+        }
+      });
+      console.log(filteredData);
+      setarticleData(filteredData);
+      console.log(response.data.data);
+    } catch (error) {
+      // Handle error
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
 
   const navigate = useNavigate();
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -17,6 +17,7 @@ import ConsultantChat from '../Pages/consultant/Consultant-Chat'
 
 //article components
 import NewArticle from '../Pages/article/NewArticle'
+import ViewArticle from '../Pages/article/ViewArticle'
 
 import Articles from '../Pages/Articles'
 import ViewConsultant from '../Pages/ViewConsultant'
@@ -73,13 +74,16 @@ function PageRoutes() {
             <Route path='/admin/viewconsultant' element={<AdminPrivateRoute><ViewConsultant/></AdminPrivateRoute>} />
             <Route path='/admin/announcement' element={<AdminPrivateRoute><AdminAnnouncement /></AdminPrivateRoute>} />
 
+            {/* Article Routes */}
+            <Route path='/newarticle' element={<NewArticle/>}/>
+            <Route path='/article' element={<ViewArticle/>}/>
 
             <Route path='/viewconsultant' element={<ViewConsultant/>}/>
             <Route path='/announcement' element={<Announcements/>} />
             <Route path='/announcement-admin' element={<AdminAnnouncement/>}/>
             <Route path='/myarticles' element={<ConArticle/>}/>
             {/* <Route path='/article' element={<ClientArticle/>}/> */}
-            <Route path='/newarticle' element={<NewArticle/>}/>
+            
             <Route path='/home' element={<Home/>}/>
         </Routes>
     </Router>
